0aa0653b1422cd1de202252360fec11e4d9696d8
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-08-08 11:53:12 -0700

    added data endpoint and handled on front end

--- getdata.php
+++ getdata.php
@@ -2,7 +2,42 @@
 
 require_once('mysql_connect.php');
 
-print_r($conn);
+$query = "SELECT * FROM `users`";
+
+$result = mysqli_query($conn, $query);
+
+$output = [
+	'success'=>false,
+	'data'=>[]
+];
+
+if($result){ //did the query work?
+	if(mysqli_num_rows($result)>0){  //did I get any results?
+		$output['success'] = true;
+		while( $row = mysqli_fetch_assoc($result)){ //get data
+			// array_push($output['data'], $row); og push
+			// $output['data'][count($output['data'])] = $row; CS push
+			$output['data'][] = $row; //push in PHP now
+		}		
+	} else {
+		//the query worked, but there was no data
+	}
+} else {
+	//the query didn't work
+}
+
+$json_output = json_encode($output);
+
+print($json_output);
+
+
+
+
+
+
+
+
+
 
 
 ?>
